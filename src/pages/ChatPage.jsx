b957c713f46f05e1dd06909e13e4a50fe79c9e18
Ajinkya Bokade <acbokade@gmail.com>
2023-03-11 12:21:25 -0800

    comments for chatpage

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -1,12 +1,13 @@
-import React, { useState, useEffect } from "react";
-import Sidebar from "../components/ChatInterface/Sidebar";
 import Chat from "../components/ChatInterface/Chat";
+import Sidebar from "../components/ChatInterface/Sidebar";
 import "../styles/chat-styles.scss";
-import { useParams } from "react-router-dom";
 import { useMutation, useQuery, gql } from "@apollo/client";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
+// Define GraphQL queries for conversations and the last message for the conversation
 export const QUERY_CONVERSATIONS = gql`
-  query Conversations{
+  query Conversations {
     me {
       conversations {
         conversation_id
@@ -41,15 +42,10 @@ export const QUERY_CONVERSATIONS = gql`
   }
 `;
 
+// Define GraphQL mutations for sending message to the conversation
 export const SEND_MESSAGE = gql`
-  mutation CreateMessage(
-    $msg_text: String!
-    $conversation: Int!
-  ) {
-    createMessage(
-      msg_text: $msg_text
-      conversation: $conversation
-    ) {
+  mutation CreateMessage($msg_text: String!, $conversation: Int!) {
+    createMessage(msg_text: $msg_text, conversation: $conversation) {
       sender {
         email
         name
@@ -64,6 +60,7 @@ export const SEND_MESSAGE = gql`
   }
 `;
 
+// Define GraphQL subscription for receiving new messages
 export const MESSAGE_SUBSCRIPTION = gql`
   subscription Message {
     message {
@@ -89,13 +86,23 @@ export const MESSAGE_SUBSCRIPTION = gql`
   }
 `;
 
-
+/**
+ * ChatPage component that displays a list of previous conversations, allows to see
+ * previous messages and send message to each of the conversation
+ * @returns {JSX.Element} The JSX element for the ChatPage component.
+ */
 const ChatPage = () => {
   const conversation_id = parseInt(useParams().conversation_id) ?? 0;
 
-  const [selectedConversation, setSelectedConversation] = useState(conversation_id);
+  const [selectedConversation, setSelectedConversation] =
+    useState(conversation_id);
 
-  let { data: conversationData, subscribeToMore, refetch: refetchConversations  } = useQuery(QUERY_CONVERSATIONS);
+  // Fetch the conversation details and set up subscription to fetch latest messages
+  let {
+    data: conversationData,
+    subscribeToMore,
+    refetch: refetchConversations,
+  } = useQuery(QUERY_CONVERSATIONS);
 
   useEffect(
     () =>
@@ -103,23 +110,37 @@ const ChatPage = () => {
         document: MESSAGE_SUBSCRIPTION,
         updateQuery: (prev, current) => {
           if (
-            current?.subscriptionData?.data?.message && 
+            current?.subscriptionData?.data?.message &&
             Object.keys(current?.subscriptionData?.data?.message).length
           ) {
-            const updatedConversation = prev.me.conversations.find(i => i.conversation_id === current.subscriptionData.data.message.conversation.conversation_id)
-            const withMessage = { 
-              ...updatedConversation, 
-              messages: [...updatedConversation.messages, current.subscriptionData.data.message], 
-            }
+            const updatedConversation = prev.me.conversations.find(
+              (i) =>
+                i.conversation_id ===
+                current.subscriptionData.data.message.conversation
+                  .conversation_id
+            );
+            // Update the conversation by adding the new message
+            const withMessage = {
+              ...updatedConversation,
+              messages: [
+                ...updatedConversation.messages,
+                current.subscriptionData.data.message,
+              ],
+            };
             return {
               me: {
                 ...prev.me,
                 conversations: [
-                  ...prev.me.conversations.filter(i => i.conversation_id !== current.subscriptionData.data.message.conversation.conversation_id),
-                  withMessage
-                ]
-              }
-            }
+                  ...prev.me.conversations.filter(
+                    (i) =>
+                      i.conversation_id !==
+                      current.subscriptionData.data.message.conversation
+                        .conversation_id
+                  ),
+                  withMessage,
+                ],
+              },
+            };
           }
           return undefined;
         },
@@ -127,8 +148,10 @@ const ChatPage = () => {
     [subscribeToMore]
   );
 
+  // Use mutation to send message
   const [sendMessageMutation] = useMutation(SEND_MESSAGE);
 
+  // Event handler to send the message on click of send button and refetch the conversation
   const sendMessage = async (msg_text) => {
     await sendMessageMutation({
       variables: {
@@ -136,7 +159,7 @@ const ChatPage = () => {
         conversation: selectedConversation,
       },
     });
-    await Promise.all([refetchConversations()])
+    await Promise.all([refetchConversations()]);
   };
 
   return (
@@ -147,15 +170,17 @@ const ChatPage = () => {
         maxHeight: "90vh",
       }}
     >
-      <Sidebar 
+      <Sidebar
         conversations={conversationData?.me?.conversations}
-        selectedConversation={selectedConversation} 
+        selectedConversation={selectedConversation}
         onSelectConversation={setSelectedConversation}
-        />
-      <Chat 
-        conversation={conversationData?.me?.conversations.find(i => i.conversation_id === selectedConversation)} 
-        sendMessage={sendMessage} 
-        />
+      />
+      <Chat
+        conversation={conversationData?.me?.conversations.find(
+          (i) => i.conversation_id === selectedConversation
+        )}
+        sendMessage={sendMessage}
+      />
     </div>
   );
 };
