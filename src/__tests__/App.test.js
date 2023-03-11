925904c9eb88f93a5dead225bb3dbcdaeb39463a
Shubham Kulkarni <shubhamkulkarni01@gmail.com>
2023-03-10 22:37:51 -0800

    add App.js test

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,14 @@
+import { render as _r, screen, act, fireEvent, waitForElementToBeRemoved, waitFor } from '@testing-library/react'
+import { render } from '../test_setup'
+import userEvent from '@testing-library/user-event'
+import '@testing-library/jest-dom'
+import App from '../App';
+
+
+test('renders signin page', () => {
+  _r(<App />);
+  
+  expect(screen.getByText(/sign in/i)).toBeInTheDocument();
+  expect(screen.getByText(/apt/i)).toBeInTheDocument();
+  expect(screen.getByText(/scout/i)).toBeInTheDocument();
+});
